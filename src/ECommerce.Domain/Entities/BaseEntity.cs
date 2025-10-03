b8d513ccf8ba0a8dee9154d5daf98ede78a6c549
Everton Ferreira <iseverton2024@gmail.com>
2025-10-03 14:47:30 -0300

    feat(domain) add base entity

--- src/ECommerce.Domain/Entities/BaseEntity.cs
+++ src/ECommerce.Domain/Entities/BaseEntity.cs
@@ -8,7 +8,7 @@ namespace ECommerce.Domain.Entities;
 
 public class BaseEntity
 {
-    public Guid Guid { get; set; } = Guid.NewGuid();
+    public Guid Id { get; set; } = Guid.NewGuid();
     public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
     public DateTime? UpdatedAt { get; set; }
     public bool IsActive { get; set; } = true;
