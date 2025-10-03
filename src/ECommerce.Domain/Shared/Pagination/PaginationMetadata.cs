c1939f07062b29dee20dbc270e76ec3cbda4e72e
Everton Ferreira <iseverton2024@gmail.com>
2025-10-03 14:48:50 -0300

    feat(pagination): implement classes for paggination support

---
+++ src/ECommerce.Domain/Shared/Pagination/PaginationMetadata.cs
@@ -0,0 +1,9 @@
+﻿namespace ECommerce.Domain.Shared.Pagination;
+
+public class PaginationMetadata
+{
+    public int PageNumber { get; set; }
+    public int TotalPages => (int)Math.Ceiling((double)TotalCount / PageSize);
+    public int PageSize { get; set; }
+    public int TotalCount { get; set; }
+}
---
+++ src/ECommerce.Domain/Shared/Pagination/PaginationResult.cs
@@ -0,0 +1,7 @@
+﻿namespace ECommerce.Domain.Shared.Pagination;
+
+public class PaginationResult<T>
+{
+    public IReadOnlyList<T> Items { get; set; } = [];
+    public PaginationMetadata Metadata { get; set; } = default!;
+}
