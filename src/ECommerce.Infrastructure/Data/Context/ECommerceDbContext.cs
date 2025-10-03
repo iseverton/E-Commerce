4a6f749dd637cc4da3637e67e6af3e57247c46f5
Everton Ferreira <iseverton2024@gmail.com>
2025-10-03 14:41:53 -0300

    feat(repository): implment product repository

---
+++ src/ECommerce.Infrastructure/Data/Context/ECommerceDbContext.cs
@@ -0,0 +1,17 @@
+﻿using ECommerce.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerce.Infrastructure.Data.Context;
+
+public class ECommerceDbContext : DbContext
+{
+    public ECommerceDbContext(DbContextOptions<ECommerceDbContext> options) : base(options)
+    {
+    }
+     public DbSet<Product> Products { get; set; }
+}
