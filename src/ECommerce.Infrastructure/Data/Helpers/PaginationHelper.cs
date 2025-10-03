2c83246a8b1711f5bb0eb62a9bce99dd4a1d1849
Everton Ferreira <iseverton2024@gmail.com>
2025-10-03 14:38:21 -0300

    feat(pagination): implement PaginationHelper for pagination support

---
+++ src/ECommerce.Infrastructure/Data/Helpers/PaginationHelper.cs
@@ -0,0 +1,29 @@
+﻿using ECommerce.Domain.Shared.Pagination;
+using Microsoft.EntityFrameworkCore;
+
+namespace ECommerce.Infrastructure.Data.Helpers;
+
+public class PaginationHelper
+{
+    public static async Task<PaginationResult<T>> CreatePaginatedAsync<T>(
+    IQueryable<T> query, int pageNumber, int pageSize)
+    {
+        var count = await query.CountAsync();
+
+        var items = await query
+            .Skip((pageNumber - 1) * pageSize)
+            .Take(pageSize)
+            .ToListAsync();
+
+        return new PaginationResult<T>
+        {
+            Metadata = new PaginationMetadata
+            {
+                PageNumber = pageNumber,
+                PageSize = pageSize,
+                TotalCount = count
+            },
+            Items = items
+        };
+    }
+}
