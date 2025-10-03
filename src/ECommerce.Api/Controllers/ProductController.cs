af56f39488f21d37043c0222283a37e20f0fbd7c
Everton Ferreira <iseverton2024@gmail.com>
2025-10-03 14:45:50 -0300

    feat(product) add product controller

---
+++ src/ECommerce.Api/Controllers/ProductController.cs
@@ -0,0 +1,41 @@
+﻿using ECommerce.Application.Queries.Products;
+using ECommerce.Domain.Entities;
+using ECommerce.Domain.Interfaces.Repositories;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ECommerce.Api.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class ProductController : ControllerBase
+{
+    private readonly IMediator _mediator;
+    private readonly IProductRepository _productRepository;
+
+    public ProductController(IProductRepository productRepository,IMediator mediator)
+    {
+        _mediator = mediator;
+        _productRepository = productRepository;
+    }
+
+    //[HttpPost]
+    //public async Task<IActionResult> Create(Product product)
+    //{
+    //    _productRepository.AddAsync(product);
+    //    if(!await _productRepository.SaveChangesAsync())
+    //    {
+    //        BadRequest("Error ao salvar no banco");
+    //    }
+    //    return Ok("Criado com sucesso");
+
+    //}
+
+    [HttpGet()]
+    public async Task<IActionResult> GetAll( [FromQuery] ListProductsQuery query, 
+        CancellationToken cancellationToken)
+    {
+        var result = await _mediator.Send(query,cancellationToken);
+        return Ok(result);
+    }
+}
