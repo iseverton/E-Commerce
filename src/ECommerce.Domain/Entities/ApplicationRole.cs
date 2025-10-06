5f73abf68d3f25731c374c78056733433d716764
Everton Ferreira <iseverton2024@gmail.com>
2025-10-06 15:23:02 -0300

    feat(domain): add ApplicationRole Class for role management

---
+++ src/ECommerce.Domain/Entities/ApplicationRole.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerce.Domain.Entities;
+
+public class ApplicationRole : IdentityRole<Guid>
+{
+    public string Description { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public DateTime? UpdatedAt { get; set; }
+    public bool IsActive { get; set; }
+    public ApplicationRole(string roleName, string description)
+    {
+        Id = Guid.NewGuid();
+        Name = roleName;
+        NormalizedName = roleName.ToUpper();
+        Description = description;
+        CreatedAt = DateTime.UtcNow;
+        IsActive = true;
+    }
+}
